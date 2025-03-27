44152eb7f9bc938a2ebb1fc2efec921547a6b491
i-arnold <ishimwearnold15@gmail.com>
2025-03-27 09:42:47 +0200

    Added a function that uses the charAt()method

--- Arnold.js
+++ Arnold.js
@@ -1,11 +1,5 @@
 function getCharacterAtIndex(str, index) {
-    if (typeof str !== 'string') {
-      return "Input must be a string.";
-    }
-    if (typeof index !== 'number' || index < 0 || index >= str.length) {
-      return "Invalid index.";
-    }
-    return str.charAt(index);
+     return str.charAt(index);
   }
   
   // Example usage:
@@ -13,5 +7,3 @@ function getCharacterAtIndex(str, index) {
   
   console.log(getCharacterAtIndex(myString, 0)); // Output: H
   console.log(getCharacterAtIndex(myString, 7)); // Output: w
-  console.log(getCharacterAtIndex(myString, 100)); //Output: Invalid index.
-  console.log(getCharacterAtIndex(123, 1)); //Output: Input must be a string.
